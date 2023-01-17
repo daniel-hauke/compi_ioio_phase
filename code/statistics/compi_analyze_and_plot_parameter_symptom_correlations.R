6b148e7f214b16a6b42e513a8a29b0ef8dd4fcb1
Murdugan <daniel.hauke@gmx.net>
2023-01-17 11:30:18 +0000

    corrected name of effect size (rho -> tau) in parameter correlation analysis

--- code/statistics/compi_analyze_and_plot_parameter_symptom_correlations.R
+++ code/statistics/compi_analyze_and_plot_parameter_symptom_correlations.R
@@ -101,7 +101,7 @@ n.symptoms = dim(PANSS)[2]
 n.comp = n.params * n.symptoms
 
 p = vector(mode = 'numeric', length = n.comp)
-rho = vector(mode = 'numeric', length = n.comp)
+tau = vector(mode = 'numeric', length = n.comp)
 parameter = vector(mode = 'character', length = n.comp)
 symptom = vector(mode = 'character', length = n.comp)
 
@@ -112,7 +112,7 @@ for (i in 1:n.params){
     cat(paste0('\n---------------------\n ',colnames(PANSS)[j], '\n---------------------\n'))
     res = cor.test(PANSS[,j], params[,i], method ="kendall")
     p[c] = res$p.value
-    rho[c] = res$estimate
+    tau[c] = res$estimate
     parameter[c] = colnames(params)[i]
     symptom[c] = colnames(PANSS)[j]
     print(res)
@@ -128,7 +128,7 @@ sign.fdr = as.numeric(p.fdr < 0.05)
 sign.bf = as.numeric(p.bf < 0.05)
 
 cat(paste0('\n---------------------------\nSummary\n---------------------------'))
-print(data.frame(parameter, symptom, rho, p, p.fdr, p.bf, sign, sign.fdr, sign.bf), row.names = F)
+print(data.frame(parameter, symptom, tau, p, p.fdr, p.bf, sign, sign.fdr, sign.bf), row.names = F)
 
 
 
@@ -208,7 +208,7 @@ n.symptoms = dim(pcl)[2]
 n.comp = n.params * n.symptoms
 
 p = vector(mode = 'numeric', length = n.comp)
-rho = vector(mode = 'numeric', length = n.comp)
+tau = vector(mode = 'numeric', length = n.comp)
 parameter = vector(mode = 'character', length = n.comp)
 symptom = vector(mode = 'character', length = n.comp)
 
@@ -220,7 +220,7 @@ for (i in 1:n.params){
     res = cor.test(pcl[,j], params[,i], method ="kendall")
     # res = kruskal.test(Y[,j] ~ m_3)
     p[c] = res$p.value
-    rho[c] = res$estimate
+    tau[c] = res$estimate
     parameter[c] = colnames(params)[i]
     symptom[c] = colnames(pcl)[j]
     print(res)
@@ -236,7 +236,7 @@ sign.fdr = as.numeric(p.fdr < 0.05)
 sign.bf = as.numeric(p.bf < 0.05)
 
 cat(paste0('\n---------------------------\nSummary\n---------------------------'))
-print(data.frame(parameter, symptom, rho, p, p.fdr, p.bf, sign, sign.fdr, sign.bf), row.names = F)
+print(data.frame(parameter, symptom, tau, p, p.fdr, p.bf, sign, sign.fdr, sign.bf), row.names = F)
 
 
 
